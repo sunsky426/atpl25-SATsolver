fc79a397d6a8904f56070873a2e6cb4f531a47ef
Ennak-alt <aj72kanne@gmail.com>
2025-12-15 10:08:07 +0100

    Added xor reductions

--- HQPlayground/srcSAT/Transform.hs
+++ HQPlayground/srcSAT/Transform.hs
@@ -31,6 +31,19 @@ orReduc e@(OR e1 e2) =
         _ -> e
 orReduc e = e
 
+xorReduc :: Exp -> Exp
+xorReduc e@(XOR e1 e2) = 
+    case (e1, e2) of 
+        (Var s1, Var s2) | s1 == s2 -> Const False 
+        (NEG (Var s1), Var s2) | s1 == s2 -> Const True
+        (Var s1, NEG (Var s2)) | s1 == s2 -> Const True
+        (Const False, e2') -> e2'
+        (e1', Const False) -> e1'
+        (Const True, e2') -> NEG e2'
+        (e1', Const True) -> NEG e1'
+        _ -> e
+xorReduc e = e
+
 negReduc :: Exp -> Exp 
 negReduc (NEG (NEG e)) = negReduc e
 negReduc (NEG (Const True)) = Const False
