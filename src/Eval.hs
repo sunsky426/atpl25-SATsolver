09dadb712af0b14e28cfa45191ab8df76d0a31c0
Hao <sunskyhao@outlook.com>
2025-12-17 16:06:33 +0100

    a naive tensor simplifer

--- src/Eval.hs
+++ src/Eval.hs
@@ -3,30 +3,52 @@ module Eval(evalProgram) where
 import Gates
 import LinAlg
 import qualified Data.Vector as V
+import Data.List (tails)
 
-type PureTensor a = V.Vector a
+type PureTensor = V.Vector Qubit
 
-type Tensor a = [PureTensor a]
+type Tensor = [PureTensor]
 
 -- |0..0> in tensor notation
-zero :: Int -> Tensor Qubit
+zero :: Int -> Tensor
 zero dim = [V.replicate dim (qubit 1 0)]
 
 -- apply all gates in a program sequentiall (via fold)
-evalProgram :: Program -> Tensor Qubit -> Tensor Qubit
+evalProgram :: Program -> Tensor -> Tensor
 evalProgram program tensor = foldl (flip evalGate) tensor program
 
 -- distributes a gate over addition to all pure tensors in a tensor
-evalGate :: Gate -> Tensor Qubit -> Tensor Qubit
+evalGate :: Gate -> Tensor -> Tensor
 evalGate gate = concatMap (evalTerm gate)
 
 -- evaluates a gate on a pure tensor, using LinAlg Module
-evalTerm :: Gate -> PureTensor Qubit -> Tensor Qubit
+evalTerm :: Gate -> PureTensor -> Tensor
 evalTerm (Only pos gate) qbs = pure $ qbs V.// [(pos, evalSingle gate (qbs V.! pos))]
 evalTerm (Ctrl ctrls target gate) qbs = [qbs, correction]
   where
     dimension = length qbs
     beta = product $ [qsnd (qbs V.! i) | i <- ctrls]
     targetVal' = beta *^ (evalSingle gate (qbs V.! target) - (qbs V.! target))
-    correction = V.replicate dimension (qubit 0 0) V.// ((target, targetVal') : zip ctrls (repeat (qubit 0 beta)))
+    correction = V.replicate dimension (qubit 0 0) V.// ((target, targetVal') : zip ctrls (repeat (qubit 0 1)))
 
+tensorSimp :: Tensor -> Tensor
+tensorSimp t = concatMap f [(x, y) | (x : ys) <- tails t, y <- ys]
+  where 
+    f (x, y) = 
+      case pureTensorSimp x y of
+        Just pureTensor -> [pureTensor]
+        Nothing -> [x, y]
+
+
+pureTensorSimp :: PureTensor -> PureTensor -> Maybe PureTensor
+pureTensorSimp t1 t2 = do
+  let isEq = V.zipWith (~=) t1 t2
+  let lastIndex = length t1 - 1
+  firstFalseIndex <- V.findIndex not isEq
+  case countFalse isEq of
+    0 -> Just $ t1 V.// [(lastIndex, 2 *^ (t1 V.! lastIndex))]
+    1 -> Just $ t1 V.// [(firstFalseIndex, (t1 V.! firstFalseIndex) + (t2 V.! firstFalseIndex))]
+    _ -> Nothing
+
+countFalse :: V.Vector Bool -> Int
+countFalse = V.length . V.filter not
--- src/LinAlg.hs
+++ src/LinAlg.hs
@@ -1,33 +1,44 @@
 {-# LANGUAGE DerivingStrategies #-}
-module LinAlg(Qubit, qubit, evalSingle, (*^), qfst, qsnd) where
+module LinAlg(Qubit, qubit, evalSingle, (*^), qfst, qsnd, ApproxEq(..)) where
 
 import Gates
 import Numeric.LinearAlgebra
 
-newtype Qubit = Qubit (Vector (Complex Double))
-  deriving newtype (Show, Num)
+class ApproxEq a where
+  approxEq :: Double -> a -> a -> Bool
+  
+  (~=) :: a -> a -> Bool
+  a ~= b = approxEq 1e-9 a b
+
+type CC = Complex Double
+
+instance ApproxEq (Vector CC) where
+  approxEq tolerence v w = size v == size w && norm_2 (v - w) < tolerence
+
+newtype Qubit = Qubit (Vector CC)
+  deriving newtype (Show, Num, ApproxEq)
 
 -- wrote some constructors and utility functions for Qubit 
 -- because I don't want to use hmatrix directly in Eval Module
-qubit :: Complex Double -> Complex Double -> Qubit
+qubit :: CC -> CC -> Qubit
 qubit a b = Qubit (2 |> [a, b])
 
-(*^) :: Complex Double -> Qubit -> Qubit
+(*^) :: CC -> Qubit -> Qubit
 z *^ (Qubit v) = Qubit $ scale z v
 
-qfst :: Qubit -> Complex Double
+qfst :: Qubit -> CC
 qfst (Qubit v) = v ! 0
 
-qsnd :: Qubit -> Complex Double
+qsnd :: Qubit -> CC
 qsnd (Qubit v) = v ! 1
 
-ii :: Complex Double
+ii :: CC
 ii = 0 :+ 1
 
--- toVector :: Qubit -> Vector (Complex Double)
+-- toVector :: Qubit -> Vector (CC)
 -- toVector (Qubit a b) = 2 |> [a, b]
 
--- toQubit :: Vector (Complex Double) -> Qubit
+-- toQubit :: Vector (CC) -> Qubit
 -- toQubit v = Qubit (v ! 0) (v ! 1)
 
 -- evaluates the effect of a single gate on a single qubit useing Matrix Semantics (I copied this from James)
