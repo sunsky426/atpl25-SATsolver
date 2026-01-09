db73c5af0510c3dddd0343ad4457e05fff0dbb27
Ennak-alt <aj72kanne@gmail.com>
2026-01-09 19:59:04 +0100

    Added number of runs

--- exe/grover.hs
+++ exe/grover.hs
@@ -3,26 +3,24 @@ import StateVector
 import LinAlg
 import Gates
 import Data.Set as S
-
+import Data.List as L
+import Debug.Trace
 
 numberOfInputQubits :: Integer
-numberOfInputQubits = 4
+numberOfInputQubits = 5
 
 -- Need output qubit
 qubits :: [Qubit]
 qubits = [qubit (sqrt 1) (sqrt 0) | _ <- [0 .. (numberOfInputQubits-1)]]
 
-
-
 inputQubits :: [Int]
 inputQubits = [0 .. fromInteger (numberOfInputQubits-2)]
 
-allQ = [0 .. fromInteger (numberOfInputQubits-1)] 
+allQ = [0 .. fromInteger (numberOfInputQubits-1)]
 
 -- amplification
 singleSolGroverCircuit :: [Gate]
 singleSolGroverCircuit = [
-    Sing H (S.fromList allQ),
 
     -- Sing X (S.fromList [0]),
 
@@ -39,7 +37,10 @@ singleSolGroverCircuit = [
     Sing H (S.fromList allQ)
   ]
 
--- numberOfRuns = floor $ (pi / 4) * sqrt numberOfInputQubits 
+numberOfRuns :: Int
+numberOfRuns = floor $ ((pi :: Float) / (4 :: Float)) * sqrt (2 ^ numberOfInputQubits)
 
 main :: IO ()
-main = putStr $ ppSV $ tensorToStateVector $ eval singleSolGroverCircuit 1 qubits
+main = putStr $ ppSV $ tensorToStateVector $ eval (
+    Sing H (S.fromList allQ) : concat (replicate (trace ("Hello" ++ show numberOfRuns) numberOfRuns) singleSolGroverCircuit)
+  ) 1 qubits
