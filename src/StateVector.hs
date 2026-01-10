f8ee23ad7df63d7db46fb1fe4d876c12dc380c36
Ennak-alt <aj72kanne@gmail.com>
2026-01-10 09:03:07 +0100

    Better pretty printing

--- src/StateVector.hs
+++ src/StateVector.hs
@@ -11,37 +11,18 @@ import Debug.Trace (trace)
 
 prob l = (sqrt . abs . unQubit)
 
--- tensorToStateVector :: [PureTensorIV] -> NL.Vector C
--- tensorToStateVector :: [PureTensorIV] -> NL.Vector C
--- tensorToStateVector :: [PureTensorIV] -> NL.Vector C
--- tensorToStateVector [PTIV _ qbs] = V.foldl (\a b -> flatten $ outer a b) ((**2) $ unQubit $ V.head qbs) (V.map ((**2) . unQubit) $ V.tail (trace (show qbs) qbs))
--- tensorToStateVector pt@(PTIV s1 qbs1 : PTIV s2 qbs2 : t) =
---   tensorToStateVector $ PTIV 1 (V.zipWith (\q1 q2 -> 0.5 *^ (s1 *^ q1 + s2 *^ q2)) qbs1 qbs2) : (trace (show pt) t)
-
 tensorToStateVector :: [PureTensorIV] -> NL.Vector C
-tensorToStateVector pt = tensorToStateVector' (trace (show pt) pt)
-  where 
+tensorToStateVector pt = trace (show $ L.length pt) $ tensorToStateVector' pt
+  where
     tensorToStateVector' [] = 0
     tensorToStateVector' pt@(PTIV s qbs : t) =
-      (1 |> [s]) * V.foldl (\a b -> flatten $ outer a b) (unQubit $ V.head qbs) (V.map (unQubit) $ V.tail qbs) 
-      + tensorToStateVector t
+      (1 |> [s]) * V.foldl (\a b -> flatten $ outer a b) (unQubit $ V.head qbs) (V.map unQubit $ V.tail qbs)
+      + tensorToStateVector' t
 
+toBin l 0 = L.replicate (l-1) '0' Prelude.++ "0"
+toBin l 1 = L.replicate (l-1) '0' Prelude.++ "1"
+toBin l n =
+    if n `mod` 2 == 1 then  toBin (l-1) (n `div` 2) Prelude.++ "1"
+    else toBin (l-1) (n `div` 2)  Prelude.++ "0"
 
--- tensorToStateVector :: [PureTensorIV] -> NL.Vector C
--- tensorToStateVector pt@(PTIV s qbs : tl) =
---         mul' tl $ abs $ outerWithFlatten ((1 |> [s]) * (unQubit . V.head) qbs) $ V.map unQubit $ V.tail (trace (show pt) qbs)
---     where
---         mul' [] res = res
---         mul' (PTIV {scalarIV = s, qbsIV = qbs} : rest) res =
---             mul' rest $ (+ res) $ abs $ outerWithFlatten ((1 |> [s]) * (unQubit . V.head) qbs) $ V.map unQubit $ V.tail qbs
---         outerWithFlatten = V.foldl (\a b -> flatten $ outer a b)
-
-tensorToStateVector _ = 0 |> []
-
-toBin 0 = "0"
-toBin 1 = "1"
-toBin n =
-    if n `mod` 2 == 1 then  toBin (n `div` 2) Prelude.++ "1"
-    else toBin (n `div` 2)  Prelude.++ "0"
-
-ppSV v = fst $ L.foldl (\(s, n) e -> (s Prelude.++ toBin n Prelude.++ ": " Prelude.++ show (e ** 2) Prelude.++ "\n", n+1)) ("", 0) $ NL.toList v
+ppSV v = fst $ L.foldl (\(s, n) e -> (s Prelude.++ toBin (round $ logBase (fromIntegral 2) (fromIntegral $ NL.size v)) n Prelude.++ ": " Prelude.++ show (e ** 2) Prelude.++ "\n", n+1)) ("", 0) $ NL.toList v
